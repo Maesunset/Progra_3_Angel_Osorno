96415215782a05baa9e89606b661df78c5b33c12
Jared Osorno <jaredlares5@gmail.com>
2023-11-21 12:05:29 -0600

    last push

---
+++ untitled/hash.h
@@ -0,0 +1,24 @@
+//
+// Created by jared on 06/11/2023.
+//
+#ifndef UNTITLED_HASH_H
+#define UNTITLED_HASH_H
+unsigned int hashFunction(std::string key, int &hashValeu[], int lenght)
+{
+
+
+}
+
+void simpleUnhashingFuncion(const std::string& key)
+{
+
+}
+
+void print(int array[], int arrayLenght)
+{
+    for (int i = 0; i < arrayLenght; ++i)
+    {
+
+    }
+}
+#endif //UNTITLED_HASH_H
---
+++ untitled/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hash.h"
+
+int main() {
+
+    std::string key;
+    key = "El lobo peres huele a obo";
+    unsigned int hashValue[key.length()];
+
+}
